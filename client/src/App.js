bc1fb8a47353c254f59ca28835eccc4edb9d9272
karnold13 <akhampaseut@ucmerced.edu>
2021-12-03 21:04:08 -0800

    header stuff

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Valorant</h1>
       <div className="Tables">
         <button class="button">Agents</button>
         <button class="button">Weapons</button>
@@ -13,13 +14,26 @@ function App() {
         <button class="button">MapRank</button>
       </div>
 
+      <br></br>
+
       <div className="Dropdown">
-        <label>Roles:</label>
+        <label>Role: </label>
         <select name="Roles" id="Roles">
           <option value="0">--Select--</option>
           <option value="1">Controller</option>
           <option value="2">Duelist</option>
           <option value="3">Initiator</option>
+          <option value="4">Sentinel</option>
+        </select>
+
+        <label>Map: </label>
+        <select name="Maps" id="Maps">
+          <option value="0">--Select--</option>
+          <option value="1">Controller</option>
+          <option value="2">Duelist</option>
+          <option value="3">Initiator</option>
+          <option value="4">Sentinel</option>
+          <option value="4">Sentinel</option>
           <option value="4">Sentinel</option>
         </select>
       </div>
