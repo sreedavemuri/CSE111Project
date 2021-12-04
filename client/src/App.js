b9d16b8b96c2c79d971aea8ff438368421df7a24
karnold13 <akhampaseut@ucmerced.edu>
2021-12-03 21:19:48 -0800

    dropdown update

--- client/src/App.js
+++ client/src/App.js
@@ -29,12 +29,12 @@ function App() {
         <label>Map: </label>
         <select name="Maps" id="Maps">
           <option value="0">--Select--</option>
-          <option value="1">Controller</option>
-          <option value="2">Duelist</option>
-          <option value="3">Initiator</option>
-          <option value="4">Sentinel</option>
-          <option value="4">Sentinel</option>
-          <option value="4">Sentinel</option>
+          <option value="1">Split</option>
+          <option value="2">Bind</option>
+          <option value="3">Ascent</option>
+          <option value="4">Icebox</option>
+          <option value="4">Breeze</option>
+          <option value="4">Haven</option>
         </select>
       </div>
     </div>
