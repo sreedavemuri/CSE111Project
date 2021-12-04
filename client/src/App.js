101da0924a9bce91c5127a82212b10bba5bf5325
karnold13 <akhampaseut@ucmerced.edu>
2021-12-03 17:33:15 -0800

    More buttons/dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,25 @@ function App() {
   return (
     <div className="App">
       <div className="Information">
-        <button>Agents</button>
-        </div>
+        <button class="button">Agents</button>
+        <button class="button">Weapons</button>
+        <button class="button">Roles</button>
+        <button class="button">KDA</button>
+        <button class="button">Map</button>
+        <button class="button">Origin</button>
+        <button class="button">MapRank</button>
+      </div>
+
+      <div className="Dropdown">
+        <label>Roles:</label>
+        <select name="Roles" id="Roles">
+          <option value="0">--Select--</option>
+          <option value="1">Controller</option>
+          <option value="2">Duelist</option>
+          <option value="3">Initiator</option>
+          <option value="4">Sentinel</option>
+        </select>
+      </div>
     </div>
   );
 }
